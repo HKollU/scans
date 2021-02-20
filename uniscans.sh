99bee3407bc47c61973ba79cfd4672a66e768a21
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-02-19 18:35:29 -0600

    Update uniscans.sh

--- uniscans.sh
+++ uniscans.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+echo "Running UniScan, this may take a while..."&&uniscan -u https://cora-vuetify-dev.herokuapp.com/login/ -qd >Uni_Scan.txt &&echo "------------------------------" && cat Uni_Scan.txt&& echo "------------------------------"
+while true
+do
+echo "done reviewing results?"
+echo "Enter anything to return to menu"
+printf "#> "
+read UNIOPT
+case #UNIOPT in
+"*")
+exit 0;
+done
