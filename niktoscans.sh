99ca00f723da272f65e0d40fb9ef0c9ae741fd63
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-02-19 18:32:50 -0600

    Update niktoscans.sh

--- niktoscans.sh
+++ niktoscans.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+echo "Running Nikto Scan, this may take a while..."&&nikto -Display 1234V -h cora-vuetify-dev.herokuapp.com >niktoScan.txt &&echo "------------------------------" && cat niktoScan.txt&& echo "------------------------------"
+while true
+do
+echo "done reviewing results?"
+echo "Enter anything to return to menu"
+printf "#> "
+read NIKOPT
+case #NIKOPT in
+"*")
+exit 0;
+done
