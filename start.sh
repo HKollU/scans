48f507482d3d1bfcd2a70bece378518d7bb1e094
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-02-22 17:00:16 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@ while true
 do
 echo "Welcome to the Penetration Test Scanning Suite Menu!"
 echo "Please enter one of the numbers as an option:"
-echo "1. NMAP scans"
-echo "2. Nikto Scans"
-echo "3. Uniscan Scan"
-echo "4. Nessus Scan"
-echo "5. Burp Suite Scan"
+echo "1. NMAP scans menu"
+echo "2. Start Nikto Scan"
+echo "3. Start Uniscan Scan"
+echo "4. Start Nessus Service (Access in browser after at https://kali:8834)"
+echo "5. Start Burp Suite"
 echo "Type exit or CTRL+C to exit program"
 printf "#> "
 read RESPONSE
