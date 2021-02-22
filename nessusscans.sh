e5aa69f29c7cf2ce50351c84265f3ee9f6839054
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-02-22 15:50:23 -0600

    Update nessusscans.sh

--- nessusscans.sh
+++ nessusscans.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+echo "STARTING NESSUS CONTAINER"
+echo "Please wat a moment.  Nessus will be available at https://kali:8834 in a web browser."
+
+docker container run -it -p 8834:8834 tenableofficial/nessus
+
 
