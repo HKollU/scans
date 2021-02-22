3bacc0eaed7383336f7e66fb530bb7cf9a168ff1
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-02-22 16:40:49 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 docker build -t dockscan .
 docker pull tenableofficial/nessus
 wget https://portswigger.net/burp/releases/download?product=community&version=2021.2.1&type=Jar
+echo "DONE BUILDING!!!"
+exit 0
