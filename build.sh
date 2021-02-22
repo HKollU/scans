4d4b82651c8f99627408085df7964b45bd108176
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-02-22 16:51:53 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ docker build -t dockscan .
 echo "Pulling relevant dockerfile(s)."
 docker pull tenableofficial/nessus
 echo "Getting burpsuite"
-$(wget https://portswigger.net/burp/releases/download?product=community&version=2021.2.1&type=Jar)
+$(wget -o https://portswigger.net/burp/releases/download?product=community&version=2021.2.1&type=Jar>burpsuite_community_v2021.2.1.jar)
 echo "DONE BUILDING!!!"
 exit 0
