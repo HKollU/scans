b76354cba78aeed6af7f5b01fcd7b8838245309a
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-02-22 19:23:49 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ docker build -t dockscan .
 echo "Pulling relevant dockerfile(s)."
 docker pull tenableofficial/nessus
 echo "Getting burpsuite.  This may take a while or appear to be frozen and it isn't."
-wget -o burpsuite_community_v2021.2.1.jar https://portswigger.net/burp/releases/download?product=community&version=2021.2.1&type=Jar
+wget -o "burpsuite_community_v2021.2.1.jar" "https://portswigger.net/burp/releases/download?product=community&version=2021.2.1&type=Jar"
+rm burpsuite_community_v2021.2.1.jar
+mv 'download?product=community&version=2021.2.1&type=Jar' burpsuite_community_v2021.2.1.jar
 echo "Done building!!!"
 exit 0
