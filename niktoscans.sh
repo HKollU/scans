80278f0c6c177b6a5b5a63f3bd3749ee9f03bdcd
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-02-22 19:14:17 -0600

    Update niktoscans.sh

--- niktoscans.sh
+++ niktoscans.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running Nikto Scan, this may take a while..."&&docker container run -it dockscan nikto -Display 1234V -h cora-vuetify-dev.herokuapp.com >niktoScan.txt &&echo "------------------------------" && cat niktoScan.txt&& echo "------------------------------"
-while true
+while true:
 do
 echo "done reviewing results?"
 echo "Enter anything to return to menu"
